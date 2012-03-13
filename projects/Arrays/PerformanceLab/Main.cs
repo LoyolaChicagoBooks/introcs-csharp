bd8c8f93d2086875a55e543cf2a9223053fe94cf
Andrew Harrington <anharrington@gmai.com>
2012-03-13 00:39:00 -0500

    pass at lab with time in seconds, no TimeSpan, table output, repetitions parameter rather than m as user parameter; expanded set discussion into own section

--- projects/Arrays/PerformanceLab/Main.cs
+++ projects/Arrays/PerformanceLab/Main.cs
@@ -7,23 +7,24 @@ namespace Arrays
    public class PerformanceLab
    {
       // chunk-experiment1-begin
-      public static TimeSpan ExperimentIntArrayLinearSearch (int n, int m, int seed)
+      public static long ExperimentIntArrayLinearSearch (int n, int rep, int seed)
       {
          Stopwatch watch = new Stopwatch ();
          int[] data = new int[n];
          Sorting.IntArrayGenerate (data, seed);
          watch.Reset ();       
          watch.Start ();
-         // perform the m lookups
-         for (int i=0; i < data.Length; i += m) {
-            var datum = Searching.IntArrayLinearSearch (data, data [i]);
+         int m = Math.Max(1, n/rep);
+         // perform the rep lookups
+         for (int k=0, i=0; k < rep; k++, i=(i+m)%n) {
+            Searching.IntArrayLinearSearch (data, data [i]);
          }
          watch.Stop ();
-         return watch.Elapsed;
+         return watch.ElapsedMilliseconds;
       }
       // chunk-experiment1-end
     
-      public static TimeSpan ExperimentIntArrayBinarySearch (int n, int m, int seed)
+      public static long ExperimentIntArrayBinarySearch (int n, int rep, int seed)
       {
          Stopwatch watch = new Stopwatch ();
          int[] data = new int[n];
@@ -32,16 +33,17 @@ namespace Arrays
          Sorting.IntArrayQuickSort (data);
          watch.Reset ();       
          watch.Start ();
-         // perform the m lookups
-         for (int i=0; i < data.Length; i += m) {
-            var datum = BinarySearching.IntArrayBinarySearch (data, data [i]);
+         int m = Math.Max(1, n/rep);
+         // perform the rep lookups
+         for (int k=0, i=0; k < rep; k++, i=(i+m)%n) {
+            BinarySearching.IntArrayBinarySearch (data, data [i]);
          }
          watch.Start ();
          watch.Stop ();
-         return watch.Elapsed;
+         return watch.ElapsedMilliseconds;
       }
           
-      public static TimeSpan ExperimentListSearch (int n, int m, int seed)
+      public static long ExperimentListSearch (int n, int rep, int seed)
       {
          Stopwatch watch = new Stopwatch ();
          int[] data = new int[n];
@@ -49,14 +51,16 @@ namespace Arrays
          List<int> dataAsList = new List<int> (data);
          watch.Reset ();       
          watch.Start ();
-         for (int i=0; i < data.Length; i += m) {
-            var datum = dataAsList.IndexOf(data [i]);
+         int m = Math.Max(1, n/rep);
+         // perform the rep lookups
+         for (int k=0, i=0; k < rep; k++, i=(i+m)%n) {
+            dataAsList.Contains(data [i]);
          }
          watch.Stop ();
-         return watch.Elapsed;
+         return watch.ElapsedMilliseconds;
       }
 
-      public static TimeSpan ExperimentSetSearch (int n, int m, int seed)
+      public static long ExperimentSetSearch (int n, int rep, int seed)
       {
          var watch = new Stopwatch ();
          var data = new int[n];
@@ -64,35 +68,67 @@ namespace Arrays
          var myset = new HashSet<int> (data);
          watch.Reset ();       
          watch.Start ();
-         for (int i=0; i < data.Length; i += m) {
-            var datum = myset.Contains(data [i]);
+         int m = Math.Max(1, n/rep);
+         // perform the rep lookups
+         for (int k=0, i=0; k < rep; k++, i=(i+m)%n) {
+            myset.Contains(data [i]);
          }
          watch.Stop ();
-         return watch.Elapsed;
+         return watch.ElapsedMilliseconds;
       }
-    
-      // chunk-printelapsedtime-begin
-      public static void PrintElapsedTime (string description, TimeSpan ts)
-      {
-         // student will customize this output to produce/calculate seconds
-         // so all timings can be procecessed, say, with Excel or another spreadsheet
-         // program
-       
-         string elapsedTime = String.Format ("{0:00}:{1:00}:{2:00}.{3:00}",
-             ts.Hours, ts.Minutes, ts.Seconds,
-             ts.Milliseconds / 10);
-         Console.WriteLine ("{0} {1}", description, elapsedTime);
-      }
-      // chunk-printelapsedtime-end
     
       public static void Main (string[] args)
       {
-         // student will write the code to parse args for the parameters
+         // Write the code to parse args for the parameters
+         // rep n1 n2 n3 ...
        
-         // student will write the code to run each of the experiments
+         // Write the code to run each of the experiments for rep and
+         // a value of n
        
-         // student will write the code to output the times and format the TimeSpan output
-         // for external processing
+         // Generate a comparative table for all values fo n specified.
       }
+      
+      /** Return a line from the keyboard in response to prompt. */
+      public static string InputLine(string prompt)
+      {
+        Console.Write(prompt);
+        return Console.ReadLine();
+      }
+  
+      // Input functions follow...
+      
+      /** True when s consists of only 1 or more digits. */
+      public static bool IsDigits(string s)
+      {
+         foreach( char ch in s) {
+            if (ch <'0' || ch > '9') {
+               return false;
+            }
+       }
+         return (s.Length > 0);
+      }
+
+      /** True if s is the string form of an int. */
+      public static bool IsIntString(string s)
+      {
+         if (s.StartsWith("-")) {
+            s = s.Substring(1);
+         }
+         return IsDigits(s);
+      }
+
+      /** Prompt the user to enter an integer until the response is legal.
+          Return the result as in int. */
+      public static int InputInt(string prompt)
+      {
+         string nStr = InputLine(prompt).Trim();
+         while (!IsIntString(nStr)) {
+            Console.WriteLine("Bad int format!  Try again.");
+            nStr = InputLine(prompt).Trim();
+         }
+         return int.Parse(nStr);
+      }
+                                                 
+
    }
 }
