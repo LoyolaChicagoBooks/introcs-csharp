8ab791063e7c0fc88bb1e28ee0ce5a8530521a60
George K. Thiruvathukal <gkt@cs.luc.edu>
2012-03-18 22:35:24 -0500

    reverting changes to Rational and use of new

--- projects/Music/Rational/RationalTests.cs
+++ projects/Music/Rational/RationalTests.cs
@@ -9,69 +9,58 @@ namespace Music
 	public class RationalTests
    {
       [Test()]
-      public void GetStructTests()
+      public void ConstructorTest()
       {
-         Rational r = Rational.GetRational(3, 5);
-         Assert.IsTrue(r.Numerator == 3);
-         Assert.IsTrue(r.Denominator == 5);
-         r = Rational.GetRational(125);
-         Assert.IsTrue(r.Numerator == 125);
-         Assert.IsTrue(r.Denominator == 1);
+         Rational r = new Rational(3, 5);
+         Assert.IsTrue(r.GetNumerator() == 3);
+         Assert.IsTrue(r.GetDenominator() == 5);
+         r = new Rational(125);
+         Assert.IsTrue(r.GetNumerator() == 125);
+         Assert.IsTrue(r.GetDenominator() == 1);
       }
 
       [Test()]
       public void ParseTest()
       {
-         Rational r, expecting;
+         Rational r;
          r = Rational.Parse("-12/30");
-
-         expecting = Rational.GetRational(-12, 30);
-         Assert.IsTrue(r.CompareTo(expecting) == 0);
+         Assert.IsTrue(r.CompareTo(new Rational(-12, 30)) == 0);
          r = Rational.Parse("123");
-         expecting = Rational.GetRational(123);
-         Assert.IsTrue(r.CompareTo(expecting) == 0);
+         Assert.IsTrue(r.CompareTo(new Rational(123)) == 0);
          r = Rational.Parse("1.125");
-         expecting = Rational.GetRational(9, 8);
-         Assert.IsTrue(r.CompareTo(expecting) == 0);
+         Assert.IsTrue(r.CompareTo(new Rational(9, 8)) == 0);
       }
 
       [Test()]
       public void BasicArithmeticTests() {
-         Rational r, r1, r2, expecting;
-         r1 = Rational.GetRational(47, 64);
-         r2 = Rational.GetRational(-11, 64);
+         Rational r, r1, r2;
+         r1 = new Rational(47, 64);
+         r2 = new Rational(-11, 64);
 
          r = r1.Add(r2);
-
-         expecting = Rational.GetRational(36, 64);
-         Assert.IsTrue(r.CompareTo(expecting) == 0);
+         Assert.IsTrue(r.CompareTo(new Rational(36, 64)) == 0);
 
          r = r1.Subtract(r2);
-         expecting = Rational.GetRational(58, 64);
-         Assert.IsTrue(r.CompareTo(expecting) == 0);
+         Assert.IsTrue(r.CompareTo(new Rational(58, 64)) == 0);
 
          r = r1.Multiply(r2);
-         expecting = Rational.GetRational(47 * -11, 64 * 64);
-         Assert.IsTrue(r.CompareTo(expecting) == 0);
-         r = r1.Divide(r2);
+         Assert.IsTrue(r.CompareTo(new Rational(47 * -11, 64 * 64)) == 0);
 
-         expecting = Rational.GetRational(47, -11);
-         Assert.IsTrue(r.CompareTo(expecting) == 0);
+         r = r1.Divide(r2);
+         Assert.IsTrue(r.CompareTo(new Rational(47, -11)) == 0);
 
          r = r1.Reciprocal();
-         expecting = Rational.GetRational(64, 47);
-         Assert.IsTrue(r.CompareTo(expecting) == 0);
+         Assert.IsTrue(r.CompareTo(new Rational(64, 47)) == 0);
 
          r = r1.Negate();
-         expecting = Rational.GetRational(-47, 64);
-         Assert.IsTrue(r.CompareTo(expecting) == 0);
+         Assert.IsTrue(r.CompareTo(new Rational(-47, 64)) == 0);
       }
 
       [Test()]
       public void BasicComparisonTests() {
-         Rational r1 = Rational.GetRational(-3, 6);
-         Rational r2 = Rational.GetRational(2, 4);
-         Rational r3 = Rational.GetRational(1, 2);
+         Rational r1 = new Rational(-3, 6);
+         Rational r2 = new Rational(2, 4);
+         Rational r3 = new Rational(1, 2);
          Assert.IsTrue(r1.CompareTo(r2) < 0);
          Assert.IsTrue(r2.CompareTo(r1) > 0);
          Assert.IsTrue(r2.CompareTo(r3) == 0);
@@ -79,8 +68,8 @@ namespace Music
 
       [Test()]
       public void ConversionTests() {
-         Rational r1 = Rational.GetRational(3, 6);
-         Rational r2 = Rational.GetRational(-3, 6);
+         Rational r1 = new Rational(3, 6);
+         Rational r2 = new Rational(-3, 6);
 
          Assert.IsTrue(r1.ToDecimal() == 0.5m);
          Assert.IsTrue(r2.ToDecimal() == -0.5m);
