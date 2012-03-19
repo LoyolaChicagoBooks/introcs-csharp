cc83ad10eddda30c1bb43df9a880edc44d5c3ab4
George K. Thiruvathukal <gkt@cs.luc.edu>
2012-03-18 22:35:24 -0500

    reverting changes to Rational and use of new

--- projects/Music/Rational/Rational.cs
+++ projects/Music/Rational/Rational.cs
@@ -7,66 +7,9 @@ namespace Music
     * including basic arithmetic operations.
     */
    public struct Rational
-   {
-      private int num, denom; // invariant: denom > 0, fraction is reduced to lowest terms
-
-      /* With structs, it does not make sense to have constructors as you can only call them on dynamically
-       * allocated instances. So we provide two static methods to get a "value" instance of Rational:
-       * GetRational(int numerator, int denominator) and GetRational(int number)
-       *
-       * The best way to work with Rational structs, however, is as follows:
-       *    Rational r;
-       *    r.Numerator = 3
-       *    r.Denominator = 6
-       *
-       * If you do not set the denominator (which starts at 0), it will be normalized the first time
-       * property Numerator is set.
-       *
-       * After this, the Rational will have the value 1/2, thanks to the existing normalize() function.
-       * These methods are provided to approximate the convenience of a constructor.
-       *
-       * All situations where we previously had "new" have been replaced by one of these two methods
-       * to ensure no wasteful dynamic allocation takes place.
-       */
-
-      public int Numerator {
-         get {
-            return num;
-         }
-         set {
-            num = value;
-            normalize();
-         }
-      }
-
-      public int Denominator {
-         get {
-            return denom;
-         }
-         set {
-            denom = value;
-            normalize();
-         }
-      }
-
-      public static Rational GetRational(int numerator, int denominator)
-      {
-         Rational r;
-         r.num = numerator;
-         r.denom = denominator;
-         r.normalize();
-         return r;
-      }
-
-      public static Rational GetRational(int numerator)
-      {
-         Rational r;
-         r.num = numerator;
-         r.denom = 1;
-         r.normalize();
-         return r;
-      }
-
+   {                          // INVARIENT:
+      private int num, denom; // denom > 0, fraction is reduced to lowest terms
+   
       /** Parse a string of an integer, fraction (with /) or decimal (with .)
        *  and return the corresponding Rational. */
       public static Rational Parse(string s)
@@ -76,24 +19,45 @@ namespace Music
          if (s.Contains("/")) {
             parts = s.Split('/');
          } else if (s.Contains(".")) {
-               parts = s.Split('.');
-               string zeros = "";
-               foreach (char dig in parts[1]) {
-                  zeros += "0";
-               }
-               parts[0] += parts[1];
-               parts[1] = "1" + zeros;
+            parts = s.Split('.');
+            string zeros = "";
+            foreach( char dig in parts[1]) {
+                zeros += "0";
             }
-
-         Rational result;
-         result.num = int.Parse(parts[0].Trim());
-         result.denom = int.Parse(parts[1].Trim());
-         result.normalize();
-         return result;
+            parts[0] += parts[1];
+            parts[1] = "1"+zeros;
+         }
+         return new Rational(int.Parse(parts[0].Trim()),
+                             int.Parse(parts[1].Trim()));
+      }
+   
+      /** Create a fraction given numerator and denominator. */
+      public Rational(int numerator, int denominator)
+      {
+         num = numerator;
+         denom = denominator;
+         normalize();
       }
 
+      public int GetNumerator()
+      {
+         return num;
+      }
 
+      public int GetDenominator()
+      {
+         return denom;
+      }
 
+      /**
+       * Create a fraction for an integer value.
+       *  num  numerator
+       */
+      public Rational(int wholeNumber):
+           this(wholeNumber, 1)  // new syntax, before body, refers to
+      {                         // other constructor using name "this"
+      }
+   
       /**
        * Return a string of the fraction in lowest terms,
        * omitting the denominator if it is 1.
@@ -102,55 +66,55 @@ namespace Music
       {
          if (denom != 1)
             return string.Format("{0}/{1}", num, denom);
-         return "" + num;
+         return ""+num;
       }
    
       /** Return a double approximation to the fraction. */
       public double ToDouble()
       {
-         return ((double)num) / denom;
+         return ((double)num)/denom;
       }
    
       /** Return a decimal approximation to the fraction. */
       public decimal ToDecimal()
       {
-         return ((decimal)num) / denom;
+         return ((decimal)num)/denom;
       }
    
       /** Return a new Rational which is this Rational negated.*/
       public Rational Negate()
       {
-         return GetRational(-num, denom);
+         return new Rational(-num, denom);
       }
    
       /** Return a new Rational which is the reciprocal of this Rational.*/
       public Rational Reciprocal()
       {
-         return GetRational(denom, num);
+         return new Rational(denom, num);
       }
    
       /** Return a new Rational which is the product of this Rational and f. */
       public Rational Multiply(Rational f)
       {
-         return GetRational(num * f.num, denom * f.denom);
+         return new Rational(num*f.num, denom*f.denom);
       }
    
       /** Return a new Rational which is the quotient of this Rational and f. */
       public Rational Divide(Rational f)
       {
-         return GetRational(num * f.denom, denom * f.num);
+         return new Rational(num*f.denom, denom*f.num);
       }
    
       /** Return a new Rational which is the sum of this Rational and f. */
       public Rational Add(Rational f)
       {
-         return GetRational(num * f.denom + denom * f.num, denom * f.denom);
+         return new Rational(num*f.denom + denom*f.num, denom*f.denom);
       }
-
+   
       /** Return a new Rational which is the difference of this Rational and f.*/
       public Rational Subtract(Rational f)
       {
-         return GetRational(num * f.denom - denom * f.num, denom * f.denom);
+         return new Rational(num*f.denom - denom*f.num, denom*f.denom);
       }
    
       /**
@@ -161,12 +125,11 @@ namespace Music
        */
       public int CompareTo(Rational f)
       {
-         return num * f.denom - denom * f.num;
+         return num*f.denom - denom*f.num;
       }
    
       /* Return the positive greatest common divisor of parameters a and b.*/
-      private static int gcd(int a, int b)
-      {  // assume not both a, b are 0
+      private static int gcd(int a, int b) {  // assume not both a, b are 0
          if (a == 0)
             return Math.Abs(b);
          while (b != 0) {  // Euclid's algorithm
@@ -178,8 +141,7 @@ namespace Music
       }
    
       /* Force the invarient: in lowest terms with a positive denominator.*/
-      private void normalize()
-      {
+      private void normalize() {
          if (denom == 0) { // We really should force an Exception, but we won't.
             Console.WriteLine("Zero denominator changed to 1!");
             denom = 1;
--- projects/Music/Rational/TestRational.cs
+++ projects/Music/Rational/TestRational.cs
@@ -9,11 +9,11 @@ namespace Music
          ShowParse("-12/30");
          ShowParse("123");
          ShowParse("1.125");
-         Rational f = Rational.GetRational(6, -10);
+         Rational f = new Rational(6, -10);
          Console.WriteLine("6/(-10): {0}", f);
          Console.WriteLine("reciprocal of {0} is {1}", f, f.Reciprocal());
          Console.WriteLine("{0} negated is {1}", f, f.Negate());
-         Rational h = Rational.GetRational(1,2);
+         Rational h = new Rational(1,2);
          Console.WriteLine("{0} + {1}: {2}", f, h, f.Add(h));
          Console.WriteLine("{0} - {1}: {2}", f, h, f.Subtract(h));
          Console.WriteLine("{0} * {1}: {2}", f, h, f.Multiply(h));
