fc3b6aaf52563c94863f345ed7b1d649325f120f
George K. Thiruvathukal" <gkt@cs.luc.edu>
2012-06-02 21:12:02 -0500

    IGame now under NAnt

---
+++ examples/igame/AdditionGame.cs
@@ -0,0 +1,36 @@
+using System;
+namespace IntroCS
+{
+   
+   class AdditionGame : IGame  // note :IGame  !!
+   {
+       private Random rand;
+       private int n;
+   
+       /**
+        * Constructor for objects of class AdditionGame
+        */
+       public AdditionGame(Random r, int big)
+       {
+           rand = r;
+           n = big;
+       }
+   
+       public int Play()  // exactly matches heading in Game interface
+       {
+           int score = 0;
+           for (int i = 0; i < 3; i++) {
+               int x = rand.Next(n), y = rand.Next(n), ans = x+y;
+               string prompt = string.Format("Enter the sum: {0} + {1} = ", x, y);
+               int val = Input.InputInt(prompt);
+               if (ans == val) {
+                   Console.WriteLine("Correct!");
+                   score++;
+               }
+               else
+                   Console.WriteLine("Wrong!  Right answer is " + ans);
+           }        
+           return score;    
+       }
+   }
+}
