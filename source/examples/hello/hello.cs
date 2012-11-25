cec0dcee64a0c564eb66b37be2c2d2c684bffd67
Andrew Harrington <anharrington@gmai.com>
2012-11-24 23:28:29 -0600

    added stuff in the intro about whitespace, also took out division by 0 in hello

--- source/examples/hello/hello.cs
+++ source/examples/hello/hello.cs
@@ -1,18 +1,9 @@
 using System;
 
-namespace hello
+class Hello
 {
-	class MainClass
+	public static void Main (string[] args)
 	{
-		public static void Main (string[] args)
-		{
-			Console.WriteLine ("Hello World!");
-			dividezero();
-		}
-
-		public static void dividezero() {
-			int zero = 0;
-			Console.WriteLine ("5 / 0 = ", 5/ zero );
-		}
+		Console.WriteLine ("Hello World!");
 	}
 }
