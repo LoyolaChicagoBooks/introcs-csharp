145ed0dc6a0c5323e5b6434c89be7fee3c884fad
George K. Thiruvathukal <thiruvathukal@gmail.com>
2013-07-15 18:07:04 -0500

    forgot to commit new scripts

---
+++ sphinx-bootstrap.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+[ -f ~/.env/sphinx/bin/activate ] && . ~/.env/sphinx/bin/activate
+
+rm -rf build/
+pushd rst
+make CONFIG=bootstrap html
+make CONFIG=bootstrap epub
+make CONFIG=bootstrap latexpdf
+popd
