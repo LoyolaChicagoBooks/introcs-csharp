4cee81c7695d65488859f0e7a76d8515df76068b
George K. Thiruvathukal <gkt@cs.luc.edu>
2014-12-20 22:52:39 -0600

    Forgot to run the git diff in the actual examples directories
    Signed-off-by: George K. Thiruvathukal <gkt@cs.luc.edu>

--- deploy-introcs-git.sh
+++ deploy-introcs-git.sh
@@ -4,10 +4,13 @@ num_changes=0
 total_checked=0
 for dir in . source/*/
 do
+	pushd $dir
 	this_changed=$(git pull | head -1 | grep -v ^Already | wc -l)
 	num_changes=$((num_changes + this_changed))
 	total_checked=$((total_checked + 1))
+	popd
 done
+
 
 [ $num_changes -gt 0 ] \
 	&& echo "$num_changes of $total_checked IntroCS repositories changed" \
