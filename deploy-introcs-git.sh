a1265ed967826f5a0119aa65e34a33ca24739aba
George K. Thiruvathukal <gkt@cs.luc.edu>
2014-12-20 22:38:50 -0600

    New deploy script that takes changes to code examples into account
    Signed-off-by: George K. Thiruvathukal <gkt@cs.luc.edu>

--- deploy-introcs-git.sh
+++ deploy-introcs-git.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-(git pull | head -1 | grep -v ^Already) \
+num_changes=0
+total_checked=0
+for dir in . source/*/
+do
+	this_changed=$(git pull | head -1 | grep -v ^Already | wc -l)
+	num_changes=$((num_changes + this_changed))
+	total_checked=$((total_checked + 1))
+done
+
+[ $num_changes -gt 0 ] \
+	&& echo "$num_changes of $total_checked IntroCS repositories changed" \
 	&& echo "git log for $(date)" \
 	&& git --no-pager log -5 --pretty=oneline \
 	&& sh ./deploy-introcs.sh
