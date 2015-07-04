5bf3667b12ea1d338b38c9ac333312aa7367b8e3
George K. Thiruvathukal <gkt@cs.luc.edu>
2015-07-04 19:32:44 +0000

    Generated gh-pages for commit e9f54dd Author: George K. Thiruvathukal <gkt@cs.luc.edu>
        make things a bit quieter

--- gh-pages.sh
+++ gh-pages.sh
@@ -4,17 +4,17 @@ git config --global user.email "gkt@cs.luc.edu"
 git config --global user.name "George K. Thiruvathukal"
 git remote add deploy "https://$GH_TOKEN@github.com/$GH_USER/$GH_REPO.git"
 git fetch deploy
-git reset deploy/gh-pages
+git reset -q deploy/gh-pages
 git checkout master source Makefile build.sh
 git reset HEAD
 rm -rf build
 ./build.sh
 ./htmlzip.sh
 mkdir -p download
-mv -vf build/html/* ./
-mv -vf build/latex/*.pdf ./download/
-mv -vf build/epub/*.epub ./download/
-mv -vf build/dist/*.zip ./download/
+mv -f build/html/* ./
+mv -f build/latex/*.pdf ./download/
+mv -f build/epub/*.epub ./download/
+mv -f build/dist/*.zip ./download/
 touch .nojekyll
 git log master -5 > COMMITS.txt
 git add -A
