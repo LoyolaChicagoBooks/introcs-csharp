9a03fbe32186ed198748431ead6e7a9c4218ddea
George K. Thiruvathukal <gkt@cs.luc.edu>
2015-07-26 15:45:21 -0500

    pull-examples.sh replacement: build.sh -g
    Signed-off-by: George K. Thiruvathukal <gkt@cs.luc.edu>

--- pull-examples.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm -rf source/*
-git clone https://github.com/LoyolaChicagoBooks/introcs-csharp-examples.git \
-	source/examples
-
-git clone https://bitbucket.org/josullivan1/comp170-xcode-xamarin-ios.git \
-	source/ios
